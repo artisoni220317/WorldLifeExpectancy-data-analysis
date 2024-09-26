ad3cd142970cc5e73cc8abd61520b57de069ca8d
Aarti Soni <artisoni8711@gmail.com>
2024-09-26 20:25:17 +0530

    Add files via upload

--- WorldLifeExpectancy data analysis.sql
+++ WorldLifeExpectancy data analysis.sql
@@ -7,6 +7,13 @@ having max(`Life expectancy`)<>0
 and min(`Life expectancy`)<>0
 order by country desc;
 
+/* Increase in Life Expectancy in 15 Years*/
+select country,MAX(`Life expectancy`),MIN(`Life expectancy`),round(MAX(`Life expectancy`)-MIN(`Life expectancy`),2) AS life_increase
+from worldlifeexpectancy group by country
+having max(`Life expectancy`)<>0
+and min(`Life expectancy`)<>0
+order by life_increase desc;
+
 /*Avg Life expectancy with gdp of the country*/
 select country,round(avg(`Life expectancy`),2) as life_exp,round(avg(GDP),1) as GDP
 from worldlifeexpectancy group by country
@@ -18,6 +25,12 @@ order by  life_exp;
 /*Avg Life expectancy with status*/
 select status,round(avg(`Life expectancy`),1) from worldlifeexpectancy
 group by status;
+
+select
+ CASE WHEN GDP>=1500 THEN 1 ELSE 0 END AS High_gdp_count,
+ AVG(CASE WHEN GDP>=1500 THEN `Life expectancy` ELSE NULL END ) high_gdp_life_expectancy
+ from worldlifeexpectancy;
+ /*Total Adult Mortality*/
 
 select country,year,`Life expectancy`,`Adult Mortality` , sum(`Adult Mortality`)
 over(partition by country order by year) as rolling_total
